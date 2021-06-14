a32f48cb902b4ff625615327fc574b73f8799dd4
saify96 <mohammad.alsaify@gmail.com>
2021-06-15 01:02:42 +0300

    lab07

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
+const express = require('express'); // require the express package
+const app = express(); // initialize your express app instance
 const cors = require('cors');
-app.use(cors()) // after you initialize your express app instance
+app.use(cors()); // after you initialize your express app instance
 require('dotenv').config();
-
+const weatherData= require('./data/weather.json');
 const port= process.env.port;
- 
-// a server endpoint 
+
+class Forecast {
+  constructor(date,desc){
+    this.date=date;
+    this.desc=desc;
+  }
+}
+
+// a server endpoint
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
- 
-app.listen(port) // kick start the express server to work
+  function (req, res) { // callback function of what we should do with our request
+    res.send('Hello World'); // our endpoint function response
+  });
+app.get('/weather',(req,res)=>{
+//   let weatherLat = req.query.lat;
+//   let weatherLon = req.query.lon;
+  const weatherArr=[];
+  weatherData.data.forEach((obj)=>{
+    let descOfDay = `Low of ${obj.low_temp}, high of ${obj.high_temp} with ${obj.weather.description}`;
+    let weatherDay = new Forecast (obj.valid_date,descOfDay);
+    weatherArr.push(weatherDay);
+  });
+  res.send(weatherArr);
+
+});
+
+app.listen(port); // kick start the express server to work
 
 
