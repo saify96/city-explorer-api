453cc8786ab16a4420191801107ac09afda93417
saify96 <mohammad.alsaify@gmail.com>
2021-06-15 17:04:14 +0300

    get the weather

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # city-explorer-api
+
+Author: Mohammad Al Saify
+
+Version: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+Overview
+website to take the city name and give the location with details to the user
+
+Getting Started
+take the input by form get the information based on the input show the information
+
+Architecture
+React, Bootstrap, CSS
+
+Change Log
+13-06-2021 6:10pm Application now able to take the city name and return the lan,lon and the map img.
+
+Credit and Collaborations
+Anas Al Ramahi Majd Khasawneh
+
--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const express = require('express'); // require the express package
 const app = express(); // initialize your express app instance
 const cors = require('cors');
+const axios = require('axios');
 app.use(cors()); // after you initialize your express app instance
 require('dotenv').config();
-const weatherData= require('./data/weather.json');
-const port= process.env.port;
+const port = process.env.port;
 
 class Forecast {
-  constructor(date,desc){
-    this.date=date;
-    this.desc=desc;
+  constructor(date, desc) {
+    this.date = date;
+    this.desc = desc;
   }
 }
-
-// a server endpoint
-app.get('/', // our endpoint name
-  function (req, res) { // callback function of what we should do with our request
-    res.send('Hello World'); // our endpoint function response
-  });
-app.get('/weather',(req,res)=>{
-//   let weatherLat = req.query.lat;
-//   let weatherLon = req.query.lon;
-  const weatherArr=[];
-  weatherData.data.forEach((obj)=>{
-    let descOfDay = `Low of ${obj.low_temp}, high of ${obj.high_temp} with ${obj.weather.description}`;
-    let weatherDay = new Forecast (obj.valid_date,descOfDay);
-    weatherArr.push(weatherDay);
-  });
-  res.send(weatherArr);
-
+app.get('/weather', (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=f049d01cee5c4a2dacd05a3c14d82f61&lat=${lat}&lon=${lon}`)
+    .then(response => {
+      const weatherArr = [];
+      response.data.data.map((obj) => {
+        let descOfDay = `Low of ${obj.low_temp}, high of ${obj.high_temp} with ${obj.weather.description}`;
+        let weatherDay = new Forecast(obj.valid_date, descOfDay);
+        weatherArr.push(weatherDay);
+      });
+      res.send(weatherArr);
+    })
+    .catch((error) => res.send(error.message));
 });
-
 app.listen(port); // kick start the express server to work
 
 
