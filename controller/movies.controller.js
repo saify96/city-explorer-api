f10fd8b40816f2f7f1dbfe4ead475f99a571062b
saify96 <mohammad.alsaify@gmail.com>
2021-06-17 16:28:57 +0300

    cache the data

--- controller/movies.controller.js
+++ controller/movies.controller.js
@@ -1,20 +1,31 @@
-const Movie =require('../models/movies.model');
+const Movie = require('../models/movies.model');
 const axios = require('axios');
 require('dotenv').config();
+const Cache = require('../helper/Cache');
+const cacheObj = new Cache();
 
 const moviesController = (req, res) => {
-  let cityName = req.query.city;
-  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.Movies_API_Key}&query=${cityName}`)
-    .then(response => {
-      const moviesArr = [];
-      response.data.results.map((item) => {
-        let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
-        let newMovie = new Movie(item.title, item.overview, item.vote_average, item.vote_count, imageURL, item.popularity, item.release_date);
-        moviesArr.push(newMovie);
-      });
-      res.send(moviesArr);
-    })
-    .catch((error) => res.send(error.message));
+  const cityName = req.query.city;
+  const requestKey = `${cityName}`;
+
+  if (cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)) {
+    res.send(cacheObj[requestKey]);
+  }
+  else {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.Movies_API_Key}&query=${cityName}`)
+      .then(response => {
+        const moviesArr = [];
+        response.data.results.map((item) => {
+          let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+          let newMovie = new Movie(item.title, item.overview, item.vote_average, item.vote_count, imageURL, item.popularity, item.release_date);
+          moviesArr.push(newMovie);
+        });
+        cacheObj[requestKey]=moviesArr;
+        cacheObj[requestKey].timestamp = Date.now();
+        res.send(moviesArr);
+      })
+      .catch((error) => res.send(error.message));
+  }
 };
 
 module.exports = moviesController;
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 app.use(cors()); // after you initialize your express app instance
 require('dotenv').config();
 const PORT = process.env.PORT;
-const weatherController = require('./controller/weather.controller')
-const moviesController = require('./controller/movies.controller')
+const weatherController = require('./controller/weather.controller');
+const moviesController = require('./controller/movies.controller');
 
 app.get('/weather', weatherController );
 app.get('/movies', moviesController );
